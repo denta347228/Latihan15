e3bcc53736123ece5600e773ee1b4aafa83b9abf
Denta Mustofa <77037397+denta347228@users.noreply.github.com>
2024-08-29 09:02:25 +0700

    AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
+      public function bot()
+    {
+        Schema::defaultStringLength(192);
+        if($this->app->environment('production'))
+        {
+
+            URL::forceScheme('https');
+
+        }
+    }
+ 
     public function register()
     {
         //
